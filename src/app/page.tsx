05ac28732ffc5e9131bf7d82d4ab31ebf43461f9
iroger <mroger.dev@gmail.com>
2024-07-01 23:52:44 -0300

    instalei o axios... add interface para buscar os dados da api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
 "use client";
-import { useRef } from "react";
+import { useState, useRef } from "react";
+import axios from "axios"
 
 export default function Home() {
   const nome = useRef<HTMLInputElement>(null);
   const endereco = useRef<HTMLInputElement>(null);
+
+  const [bd, setBd] = useState<IDados[]>([]);
+
+  interface IDados {
+    id: number;
+    name: string;
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      // console.log('Dados:', response.data);
+      setBd(response.data)
+      console.log(bd)
+
+    } catch (error:any) {
+      console.error('Erro ao fazer a requisição:', error.message);
+    }
+  }
+
+  const user = {nome, endereco} // Para enviar no POST!!!
+  const dataPost = async () => {
+    try {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/users', user);
+      console.log('Dados:', response.data);
+      setBd(response.data)
+      console.log(bd)
+
+    } catch (error:any) {
+      console.error('Erro ao fazer a requisição:', error.message);
+    }
+  }
 
   async function enviar() {
     //Ela precisa ser asyncrona porque ela busca nos servidor backend
@@ -21,21 +54,32 @@ export default function Home() {
   }
 
   return (
-    <main className="flex justify-center">
+    <div>
+    <main className="flex justify-center items-center flex-col ">
       <div className="flex flex-col w-[300px]">
         <form className="flex flex-col">
           <div className="flex justify-center">
             <h1>Cadastro do usuário</h1>
           </div>
           <label htmlFor="nome">Nome: </label>
-          <input name="nome" type="text" ref={nome} />
+          <input name="nome" type="text" ref={nome}/>
           <label htmlFor="endereco">Endereço: </label>
-          <input name="endereco" type="text" ref={endereco} />
+          <input name="endereco" type="text" ref={endereco}/>
           <button type="button" onClick={enviar}>
             Enviar Pedido
           </button>
+          <button type="button" onClick={fetchData}>
+            Teste
+          </button>
         </form>
       </div>
+      {bd.map((user) => (
+        <div key={user.id} className="flex justify-center items-center gap-4">
+          <p>{user.id}</p>
+          <p>{user.name}</p>
+        </div>
+      ))}
     </main>
+  </div>
   );
 }
